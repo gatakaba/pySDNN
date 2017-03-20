9b0c8f47f1020c6fe5894b0e2dd1a478580adb42
gatakaba <m07112@gmail.com>
2017-03-21 08:47:28 +0900

    update docs

--- tests/test_coding.py
+++ tests/test_coding.py
@@ -9,20 +9,21 @@ from pysdnn.coding import SelectiveDesensitization
 
 class TestCoding(unittest.TestCase):
     def setUp(self):
-        self.binary_vector_dim = 8
+        self.code_pattern_dim = 8
         self.division_num = 6
         self.reversal_num = 2
 
-        self.pc = PatternCoding(self.binary_vector_dim, self.division_num, self.reversal_num)
+        self.pc = PatternCoding(self.code_pattern_dim, self.division_num, self.reversal_num)
         self.feature_num = 5
-        self.pc.make_binary_vector_table(self.feature_num)
+        self.pc.make_code_pattern_table(self.feature_num)
 
-    def test_array_size(self):
-        assert self.pc._make_binary_vector_table_1d().shape == (self.division_num, self.binary_vector_dim)
-        assert self.pc.binary_vector_table.shape == (self.feature_num, self.division_num, self.binary_vector_dim)
+    def test_code_pattern_size(self):
+        assert self.pc._make_binary_vector_table_1d().shape == (self.division_num, self.code_pattern_dim)
+
+        assert self.pc.code_pattern_table.shape == (self.feature_num, self.division_num, self.code_pattern_dim)
 
     def test_num_to_pattern(self):
         # check 1d
         x = np.random.random(size=self.feature_num)
         # check size
-        assert self.pc.num_to_pattern(x).shape == (self.binary_vector_dim * self.feature_num,)
+        assert self.pc.coding(x).shape == (self.code_pattern_dim * self.feature_num,)
