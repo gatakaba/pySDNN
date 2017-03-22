c517fcb0f29b7a789d1c5cda15a6fc3f4d8ceb80
gatakaba <m07112@gmail.com>
2017-03-22 11:37:57 +0900

    update docs

--- pysdnn/parallel_perceptron.py
+++ pysdnn/parallel_perceptron.py
@@ -1,16 +1,37 @@
-# coding:utf-8
-"""
-PP
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+""" Copyright (C) 2017 Yu Kabasawa
+
+This is licensed under an MIT license. See the readme.md file
+for more information.
 
 """
+
 import numpy as np
 from sklearn.base import BaseEstimator
 from sklearn.utils.validation import check_X_y, check_is_fitted
-from pysdnn.utilities import add_columns
+from pysdnn.utils import add_columns
 
 
 class PP(BaseEstimator):
-    def __init__(self, hidden_layer_num=300, eta=10 ** -3, verbose=False):
+    """ PPは複数のパーセプトロンを並列に並べ,それらの出力値の総計に応じて最終的な出力決定する教師あり学習モデルである.
+
+    PPは3層のMLPにおいて,中間層の活性化関数をヘビサイド関数にし,
+    中間層から出力層の結合荷重を固定したものとみなすことができる.
+
+    Parameters
+    ----------
+    hidden_layer_num : int
+        中間層の素子数
+    eta: float
+        学習係数
+    is_pattern : bool
+        パターンコードに変換した値を入力に用いる
+    verbose : bool
+        詳細な出力を有効にする
+    """
+
+    def __init__(self, hidden_layer_num=300, eta=10 ** -3, is_pattern=False, verbose=False):
         self.W = None
         self.n_samples = None
         self.n_features = None
@@ -19,6 +40,7 @@ class PP(BaseEstimator):
         self.verbose = verbose
         self.a = 1.4 / self.hidden_layer_num
         self.b = -0.2
+        self.is_pattern = is_pattern
 
     @staticmethod
     def hidden_function(x):
@@ -32,7 +54,8 @@ class PP(BaseEstimator):
         x = (y - self.b) / self.a
         return x
 
-    def _search_index(self, a, n_target, n_predict):
+    @staticmethod
+    def _search_index(a, n_target, n_predict):
         # 修正するパーセプトロンを選ぶ
         error_num = int(round(np.abs(n_target - n_predict)))
 
@@ -72,7 +95,7 @@ class PP(BaseEstimator):
 
         for j in range(100):
             for i in (range(n_samples)):
-                # feedforward
+                # 順伝播
                 a = np.dot(self.W, intercepted_X[i])
                 z = self.hidden_function(a)
                 n_predict = np.sum(z)
@@ -102,6 +125,9 @@ class PP(BaseEstimator):
             prediction_list.append(prediction)
         y = np.ravel(prediction_list)
         return y
+
+    def score(self):
+        pass
 
 
 '''
--- tests/test_pp.py
+++ tests/test_pp.py
@@ -1,32 +1,28 @@
 import numpy as np
 import unittest
 
-from pysdnn import PP
-from sklearn.utils.estimator_checks import check_estimator
+from pysdnn.parallel_perceptron import PP
 
 
-class Test_PP(unittest.TestCase):
+class TestPP(unittest.TestCase):
     def setUp(self):
         # make sample data
-        n_train_samples = 200
-        n_test_samples = 100
+        n_train_samples = 20
+        n_test_samples = 10
         self.train_X = np.random.uniform(0, 1, size=[n_train_samples, 3])
         self.train_y = np.random.normal(size=n_train_samples)
 
         self.test_X = np.random.uniform(0, 1, size=[n_test_samples, 3])
         self.test_y = np.random.normal(size=n_train_samples)
 
-        self.pp = PP()
+    def test_pattern(self):
+        pass
 
     def test_fit(self):
+        self.pp = PP()
         self.pp.fit(self.train_X, self.train_y)
 
     def test_predict(self):
+        self.pp = PP()
         self.pp.fit(self.train_X, self.train_y)
         self.pp.predict(self.test_X)
-
-    """
-    def test_estimator(self):
-        check_estimator(MultiLayerPerceptronRegression)
-
-    """
