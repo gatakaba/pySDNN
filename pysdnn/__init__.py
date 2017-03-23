0ec8e15c52d95867b702e5dee9ac0ea5d873b020
gatakaba <m07112@gmail.com>
2017-03-23 11:32:47 +0900

    fix path

--- pysdnn/__init__.py
+++ pysdnn/__init__.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from pysdnn.base_network import BaseNetwork
-from pysdnn.parallel_perceptron import PP_A, PP_P
-from pysdnn.selective_desensitization_neural_network import SDNN
+from .base_network import BaseNetwork
+from .parallel_perceptron import PP_A, PP_P
+from .selective_desensitization_neural_network import SDNN
 
 
 # from pysdnn.multi_layer_perceptron import MLP
