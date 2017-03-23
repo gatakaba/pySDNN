2c79f51a8ddc68486bd9ceb5d2c259e7bb4b8de1
gatakaba <m07112@gmail.com>
2017-03-23 11:58:31 +0900

    move sample function to utils

--- pysdnn/__init__.py
+++ pysdnn/__init__.py
@@ -4,7 +4,7 @@
 from .base_network import BaseNetwork
 from .parallel_perceptron import PP_A, PP_P
 from .selective_desensitization_neural_network import SDNN
-import .sample_function
+
 
 # from pysdnn.multi_layer_perceptron import MLP
 # from simple_perceptron import SP
